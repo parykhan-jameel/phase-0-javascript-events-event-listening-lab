b783fcdc84c90fe350b7ace5886d99ae0c6b8614
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-13 18:33:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+  
+
+    const input = document.querySelector('input');
+    input.addEventListener('click', function() {
+    alert('I was clicked!');
+    });
+    
 }
